caf1f57b98be001b88a31294554e612d020c8ce7
Paulo Vinícius Toledo <pvvtoledo@gmail.com>
2025-09-03 22:54:23 -0300

    chore: hero section links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,8 +6,8 @@ export function Header() {
     <header className="flex items-center h-20 border-b border-b-foreground/10">
       <div className="flex items-center justify-between w-full max-w-7xl mx-auto px-10 lg:px-0">
         <div className="flex items-end gap-1.5 lg:gap-2.5">
-          <Icons.code  strokeWidth={3} className="mb-0.5 w-5 h-5 lg:w-9 lg:h-9 lg:mb-1" />
-          <span className="text-xl font-heading leading-none lg:text-4xl">
+          <Icons.code  strokeWidth={3} className="mb-0.5 w-5 h-5 lg:w-9 lg:h-9" />
+          <span className="text-xl font-heading leading-none lg:text-3xl">
             Paulo Vinícius
           </span>
         </div>
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,7 +1,9 @@
-import { CodeXml, MapPin, MonitorCog } from "lucide-react";
+import { CodeXml, Download, Github, MapPin, MonitorCog } from "lucide-react";
 
 export const Icons = {
     code: CodeXml,
     location: MapPin,
     engineering: MonitorCog,
+    download: Download,
+    github: Github
 }
