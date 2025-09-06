7a35d918feb77dbdff505266c975b03f6c20c576
Paulo Vinícius Toledo <pvvtoledo@gmail.com>
2025-09-06 12:40:52 -0300

    feat: about section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,11 +1,13 @@
+import { About } from "@/components/about";
 import { Header } from "@/components/header";
 import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <>
+    <main className="flex flex-col pb-40">
       <Header />
       <HeroSection />
-    </>
+      <About />
+    </main>
   );
 }
---
+++ src/components/about.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { AspectRatio } from "./ui/aspect-ratio";
+
+export function About() {
+  return (
+    <section className="flex flex-col gap-7.5 w-full max-w-7xl mx-auto px-10 mt-20 lg:flex-row xl:px-0 lg:mt-40">
+      <div className="flex flex-col w-full lg:max-w-[50%] gap-7.5 lg:gap-10">
+        <h4 className="text-2xl font-heading lg:text-4xl">
+          Devolping efficient and modern applications
+        </h4>
+        <p className="lg:text-lg text-justify">
+          Full Stack Developer building high-quality applications with React,
+          Next.js, and Node.js, with real-world experience taking freelance
+          projects from concept to client delivery. Focused on solving problems
+          by engineering robust solutions that provide a smooth and captivating
+          user experience within the modern TypeScript ecosystem.
+        </p>
+      </div>
+      <div className="w-full lg:max-w-[50%]">
+        <AspectRatio ratio={16 / 9}>
+          <Image
+            src="/profile-picture.jpg"
+            fill
+            alt=""
+            className="object-cover rounded-xl"
+          />
+        </AspectRatio>
+      </div>
+    </section>
+  );
+}
